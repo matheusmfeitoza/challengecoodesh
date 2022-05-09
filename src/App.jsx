278ff945e1bd02e903ed038b428b2dfa7449ca88
Matheus Marinho <feitozamatheus123@gmail.com>
2022-05-08 22:33:31 -0300

    Created Slider, need some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,100 @@
-import { useState } from "react";
-import * as ChartGeo from "chartjs-chart-geo";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Slider } from "@mui/material";
 
+const marks = [
+  {
+    value: 0,
+    label: "jan/2020",
+    interval: {
+      start: "2021-04-05",
+      end: "2021-05-05",
+    },
+  },
+  {
+    value: 25,
+    label: "jul/2020",
+    interval: {
+      start: "2021-06-05",
+      end: "2021-07-05",
+    },
+  },
+  {
+    value: 55,
+    label: "jan/2021",
+    interval: {
+      start: "2021-08-05",
+      end: "2021-09-05",
+    },
+  },
+  {
+    value: 75,
+    label: "jul/2021",
+    interval: {
+      start: "2021-10-05",
+      end: "2021-12-05",
+    },
+  },
+  {
+    value: 100,
+    label: "jan/2022",
+    interval: {
+      start: "2022-01-05",
+      end: "2022-02-05",
+    },
+  },
+];
 export function App() {
+  let today = new Date();
   const [dados, setDados] = useState(null);
   const [getInputvalue, setGetInputValue] = useState("");
+  const [mark, setMark] = useState(marks[0]);
+
+  const valuetext = (value) => {
+    return `${value}`;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    const response = await fetch("http://localhost:3000/covid");
-    const date = await response.json();
   };
+
+  const handleSliderOnChange = (_, value) => {
+    console.log(value);
+    const selectedMark = marks.find((mark) => mark.value === value);
+    setMark(selectedMark);
+  };
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/covid", {
+        params: {
+          _sort: "date",
+          _order: "asc",
+          _limit: 10,
+          date_gte: mark.interval.start,
+          date_lte: mark.interval.end,
+        },
+      })
+      .then((r) => console.log(r));
+  }, [mark.interval.start, mark.interval.end]);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={getInputvalue}
-          onChange={(event) => {
-            setGetInputValue(event.target.value);
-          }}
+        <button>teste</button>
+        <button>teste</button>
+        <button>teste</button>
+        <Slider
+          aria-label="Custom marks"
+          defaultValue="0"
+          getAriaValueText={(currentMark) => currentMark.label}
+          step={0}
+          value={mark.value}
+          valueLabelDisplay="auto"
+          marks={marks}
+          onChange={handleSliderOnChange}
         />
-        <input type="range" min="100" max="200" />
         <button>Buscar dados</button>
       </form>
-      {dados && dados.map((item) => {})}
     </div>
   );
 }
